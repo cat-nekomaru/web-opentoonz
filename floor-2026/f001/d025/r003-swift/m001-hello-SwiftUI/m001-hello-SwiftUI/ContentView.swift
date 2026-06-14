46ab3ce298fbec1ed82dc352a9dca9ca3fc47bf1
Nekomaru <yoji.nekomaru@gmail.com>
2026-06-15 06:41:17 +0900

    docs: Day 25（d025）のREADME.md を更新

---
+++ floor-2026/f001/d025/r003-swift/m001-hello-SwiftUI/m001-hello-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  m001-hello-SwiftUI
+//
+//  Created by Cat Nekomaru on 2026/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ floor-2026/f001/d025/r003-swift/m001-hello-SwiftUI/m001-hello-SwiftUI/m001_hello_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  m001_hello_SwiftUIApp.swift
+//  m001-hello-SwiftUI
+//
+//  Created by Cat Nekomaru on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct m001_hello_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
